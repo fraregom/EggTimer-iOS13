8ca106fea4dbf1b5cb631407c7951709f67035d9
Francisco Reyes <francisco.reyesgom@gmail.com>
2022-11-20 19:12:04 -0300

    Finished mini project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,30 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
         
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 1, "Medium": 3, "Hard": 4]
     @IBOutlet weak var titleEggTimer: UILabel!
+    @IBOutlet weak var barProgress: UIProgressView!
     var timer = Timer()
+    var player: AVAudioPlayer!
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
     
     func timer(time: Int){
         timer.invalidate()
+        barProgress.progress = 0.0
+        let timeTarget: Int = time * 30
+        var secondsPassed: Int = 0
         
-        var secondsRemaining: Int = time // * 60
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-                if secondsRemaining > 0 {
-                    print ("\(secondsRemaining) seconds")
-                    secondsRemaining -= 1
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
+            (Timer) in
+                if secondsPassed < timeTarget {
+                    secondsPassed += 1
+                    self.barProgress.progress = Float(secondsPassed)/Float(timeTarget)
                 } else {
                     Timer.invalidate()
                     self.titleEggTimer.text = "Done!"
+                    self.playSound()
                 }
             }
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle
+        self.titleEggTimer.text = sender.currentTitle
         
         switch hardness {
             case "Soft":
